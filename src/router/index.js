bdc9180a6d58fa5d78c01beec86108343ffc1df2
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-07-29 15:52:33 +0800

    feat: add sign in feature

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
     path: '*',
     name: 'not-found',
-    component: NotFound,
-  },
+    component: NotFound
+  }
 ]
 
 const router = new VueRouter({
