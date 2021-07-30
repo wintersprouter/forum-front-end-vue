bbafbad99fac3aebca3ae32cfa34164f02c99a70
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-07-30 17:19:09 +0800

    feat: add TopUsers component

---
+++ src/components/TopUsers.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="col-3">
+  <a href="#">
+          <img
+            :src="user.image"
+            :alt="user.name"
+            width="140px"
+            height="140px"
+          >
+        </a>
+        <h2>{{user.name}}</h2>
+        <span class="badge badge-secondary">追蹤人數：{{user.Followers.length}}</span>
+        <p class="mt-3">
+          <button
+            type="button"
+            class="btn btn-danger"
+          >
+            取消追蹤
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+          >
+            追蹤
+          </button>
+        </p>
+      </div>
+</template>   
+<script>
+export default {
+  props: {
+    initialUser: {
+      type: Object,
+      required: true
+    }
+  },
+  data(){
+    return{
+      user: this.initialUser
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
