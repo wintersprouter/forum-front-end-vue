440a422523f9a9c7ce575811a94e7f245c444ddb
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-07-30 17:29:48 +0800

    feat: show follow staus by converting follow/unfollow buttons

--- src/components/TopUsers.vue
+++ src/components/TopUsers.vue
@@ -1,32 +1,37 @@
 <template>
-<div class="col-3">
-  <a href="#">
-          <img
-            :src="user.image"
-            :alt="user.name"
-            width="140px"
-            height="140px"
-          >
-        </a>
-        <h2>{{user.name}}</h2>
-        <span class="badge badge-secondary">追蹤人數：{{user.Followers.length}}</span>
-        <p class="mt-3">
-          <button
-            type="button"
-            class="btn btn-danger"
-          >
-            取消追蹤
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary"
-          >
-            追蹤
-          </button>
-        </p>
-      </div>
+  <div class="col-3">
+    <a href="#">
+        <img
+        :src="user.image"
+        :alt="user.name"
+        width="140px"
+        height="140px"
+        >
+    </a>
+    <h2>{{user.name}}</h2>
+      <span class="badge badge-secondary">追蹤人數：{{user.Followers.length}}</span>
+    <p class="mt-3">
+      <button
+      v-if = "user.isFollowed"
+      type="button"
+      class="btn btn-danger"
+      @click.stop.prevent="unfollow"
+      >
+        取消追蹤
+      </button>
+      <button
+      v-else
+      type="button"
+      class="btn btn-primary"
+      @click.stop.prevent="follow"
+      >
+      追蹤
+      </button>
+    </p>
+  </div>
 </template>   
 <script>
+
 export default {
   props: {
     initialUser: {
@@ -34,13 +39,24 @@ export default {
       required: true
     }
   },
-  data(){
-    return{
+  data () {
+    return {
       user: this.initialUser
     }
   },
   methods:{
-
+    follow () {
+      this.user = {
+        ...this.user,
+        isFollowed:true
+      }
+    },
+    unfollow () {
+      this.user = {
+        ...this.user,
+        isFollowed:false
+      }
+    },
   }
 }
 </script>
