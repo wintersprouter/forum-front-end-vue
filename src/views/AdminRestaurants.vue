d394ef01953a47257e80daa11ce9834e0f0c71e5
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-07-31 17:12:29 +0800

    feat: add AdminRestaurantsTable component

--- src/views/AdminRestaurants.vue
+++ src/views/AdminRestaurants.vue
@@ -10,15 +10,18 @@
     </a>
 
     <!-- 後台餐廳列表 AdminRestaurantsTable -->
+    <AdminRestaurantsTable/>
   </div>
 </template>
 <script>
 
 import AdminNav from './../components/AdminNav'
+import AdminRestaurantsTable from './../components/AdminRestaurantsTable'
 export default {
   name:"AdminRestaurants",
   components: {
-    AdminNav
+    AdminNav,
+    AdminRestaurantsTable
   }
 }
 </script>
