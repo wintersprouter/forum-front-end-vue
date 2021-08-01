d1b0ba6a1df96061f81d5e90c710ee4f321cfce4
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-01 15:36:48 +0800

    feat: add edit and save features

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -45,24 +45,50 @@
         </tr>
       </thead>
       <tbody>
-        <tr
-          v-for="category in categories"
-          :key="category.id"
-        >
-          <th scope="row">
-            {{ category.id }}
-          </th>
+      <tr
+        v-for="category in categories"
+        :key="category.id"
+      >
+      <th scope="row">
+        {{ category.id }}
+      </th>
           <td class="position-relative">
-            <div class="category-name">
+            <div
+              v-show="!category.isEditing"
+              class="category-name"
+            >
               {{ category.name }}
             </div>
+            <input
+              v-show="category.isEditing"
+              v-model="category.name"
+              type="text"
+              class="form-control"
+            >
+            <span
+              v-show="category.isEditing"
+              class="cancel"
+              @click="handleCancel(category.id)"
+            >
+              ✕
+            </span>
           </td>
           <td class="d-flex justify-content-between">
             <button
+              v-show="!category.isEditing"
               type="button"
               class="btn btn-link mr-2"
+              @click.stop.prevent="toggleIsEditing(category.id)"
             >
               Edit
+            </button>
+            <button
+              v-show="category.isEditing"
+              type="button"
+              class="btn btn-link mr-2"
+              @click.stop.prevent="updateCategory({ categoryId: category.id, name: category.name })"
+            >
+              Save
             </button>
             <button
               type="button"
@@ -130,24 +156,90 @@ export default {
   methods: {
     // 4. 定義 `fetchCategories` 方法，把 `dummyData` 帶入 Vue 物件
     fetchCategories () {
-      this.categories = dummyData.categories
+      //retrun object 需在外圍加一個()
+      this.categories = dummyData.categories.map(category => ({
+        ...category,
+        isEditing:false,
+        nameCached: ''
+      }))
     },
     creatCategory () {
-      // TODO: 透過 API 告知伺服器欲新增的餐廳類別...
-      // 將新的類別添加到陣列中
-      this.categories.push({
-        id: uuidv4(),
-        name: this.newCategoryName
+        // TODO: 透過 API 告知伺服器欲新增的餐廳類別...
+        // 將新的類別添加到陣列中
+        this.categories.push({
+          id: uuidv4(),
+          name: this.newCategoryName
+        })
+        this.newCategoryName = '' // 清空原本欄位中的內容
+      },
+      deleteCategory(categoryId) {
+        //TODO: 透過 API 告知伺服器欲刪除的餐廳類別
+        // 將該餐廳類別從陣列中移除
+        this.categories = this.categories.filter(
+          category => category.id !== categoryId
+        )
+      },
+      toggleIsEditing (categoryId) {
+        this.categories = this.categories.map(category => {
+          if (category.id === categoryId) {
+            return {
+              ...category,
+              isEditing: !category.isEditing,
+              //編輯前的名稱
+              nameCached: category.name
+            }
+          }
+          return category
+        })
+      }, 
+      updateCategory ({ categoryId, name }) {
+        // TODO: 透過 API 去向伺服器更新餐廳類別名稱
+        this.toggleIsEditing(categoryId)
+      },
+      handleCancel (categoryId) {
+      this.categories = this.categories.map(category => {
+        if (category.id === categoryId) {
+          return {
+            ...category,
+            // 把原本的餐廳類別名稱還回去
+            name: category.nameCached
+          }
+        }
+        return category
       })
-      this.newCategoryName = '' // 清空原本欄位中的內容
-    },
-    deleteCategory(categoryId) {
-      // TODO: 透過 API 告知伺服器欲刪除的餐廳類別
-      // 將該餐廳類別從陣列中移除
-      this.categories = this.categories.filter(
-        category => category.id !== categoryId
-      )
-    }
-  }
+      this.toggleIsEditing(categoryId)
+    }  
+  },
 }
 </script>
+
+<style scoped>
+/* scoped只會作用到當下的這一個 component */
+.category-name {
+  padding: 0.375rem 0.75rem;
+  border: 1px solid transparent;
+  outline: 0;
+  cursor: auto;
+}
+
+.btn-link {
+  width: 62px;
+}
+
+.cancel {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #aaaaaa;
+  border-radius: 50%;
+  user-select: none;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>
