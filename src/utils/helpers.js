37451ff1858289f2f6cfbbf3cdf7eee55dbac9d3
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-01 19:44:20 +0800

    feat: fetch signin api

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const baseURL = 'https://forum-express-api.herokuapp.com/api'
 
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -58,6 +58,8 @@
   </div>
 </template>
 <script>
+import authorizationAPI from './../apis/authorization'
+
 export default {
   data(){
     return {
@@ -67,13 +69,18 @@ export default {
   },
   methods: {
     handleSubmit () {
-      const data = JSON.stringify({
+      authorizationAPI.signIn({
         email: this.email,
         password: this.password
-      })
+      }).then(response => {
+        // 取得 API 請求後的資料
+        const { data } = response
+        // 將 token 存放在 localStorage 內
+        localStorage.setItem('token', data.token)
 
-      // TODO: 向後端驗證使用者登入資訊是否合法
-      console.log('data', data)
+        // 成功登入後轉址到餐廳首頁
+        this.$router.push('/restaurants')
+      })
     }
   }
 }
