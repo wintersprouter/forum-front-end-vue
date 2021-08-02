91b7b0a1cd2e8d8f2f62685835ebd4b1ae8d6356
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-02 14:51:53 +0800

    style: Use JavaScript Standard Style

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="container py-5">
+  <div class="container py-5">
     <NavTabs />
     <h1 class="mt-5">
-    最新動態
+      最新動態
     </h1>
-    <hr>
+    <hr />
     <div class="row">
-    <div class="col-md-6">
-    <h3>最新餐廳</h3>
-    <NewestRestaurants :restaurants="restaurants"/>
+      <div class="col-md-6">
+        <h3>最新餐廳</h3>
+        <NewestRestaurants :restaurants="restaurants" />
+      </div>
+      <div class="col-md-6">
+        <h3>最新評論</h3>
+        <NewestComments :comments="comments" />
+      </div>
     </div>
-    <div class="col-md-6">
-    <h3>最新評論</h3>
-    <NewestComments  :comments="comments"/>   
-    </div>
-    </div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -24,37 +24,37 @@ import NewestRestaurants from './../components/NewestRestaurants.vue'
 import NewestComments from './../components/NewestComments.vue'
 import feedsAPI from './../apis/feeds'
 import { Toast } from './../utils/helpers'
+
 export default {
-    components: {
+  components: {
     NavTabs,
     NewestRestaurants,
     NewestComments
-    },
-    data () {
+  },
+  data () {
     return {
     restaurants: [],
     comments: []
-    }
+  }
 },
 created () {
-this.fetchFeeds()
+  this.fetchFeeds()
 },
 methods: {
-    async fetchFeeds () {
-            try {
-                const response = await feedsAPI.getFeeds()
-                const { restaurants, comments } = response.data
-                this.restaurants = restaurants
-                this.comments = comments.filter(comment => comment.Restaurant && comment.text)
-            } catch (error) {
-                Toast.fire({
-                icon: 'error',
-                title: '無法取得餐廳資料，請稍後再試'
-                })
-            console.log('error', error)
-                
-            }
-        }
-    }  
+  async fetchFeeds () {
+    try {
+      const response = await feedsAPI.getFeeds()
+      const { restaurants, comments } = response.data
+      this.restaurants = restaurants
+      this.comments = comments.filter(comment => comment.Restaurant && comment.text)
+    } catch (error) {
+      Toast.fire({
+        icon: 'error',
+        title: '無法取得餐廳資料，請稍後再試'
+      })
+      console.log('error', error)
+      }
+    }
+  }
 }
 </script>
