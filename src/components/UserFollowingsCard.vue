019ae6f7ee2ab99b14d0c58f9368ba8ca8cf1d94
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-03 17:23:50 +0800

    feat: featch user api

--- src/components/UserFollowingsCard.vue
+++ src/components/UserFollowingsCard.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="card">
     <div class="card-header">
-      <strong>{{followings.length}}</strong> followings (追蹤者)
+      <strong>{{ followings.length }}</strong> followings (追蹤者)
     </div>
     <div class="card-body">
       <router-link
         v-for="following in followings"
         :key="following.id"
-        :to="{name: 'user', params: { id: following.id }}"
+        :to="{ name: 'user', params: { id: following.id } }"
       >
-      <img :src="following.image"  width="60" height="60" class="avatar">
+        <img
+          :src="following.image | emptyImage"
+          width="60"
+          height="60"
+          class="avatar"
+        />
       </router-link>
     </div>
   </div>
 </template>
 <script>
+import { emptyImageFilter } from './../utils/mixins'
+
 export default {
-  name:"UserFollowingsCard",
+  name: 'UserFollowingsCard',
+  mixins: [emptyImageFilter],
   props: {
     followings: {
       type: Array,
       default: () => []
     }
-  },
+  }
 }
 </script>
--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -2,30 +2,43 @@
   <div class="card mb-3">
     <div class="row no-gutters">
       <div class="col-md-4">
-        <img 
-        :src="profile.image"
-        alt="Card image cap" 
-        width="300px" 
-        height="300px">
+        <img
+          :src="user.image | emptyImage"
+          alt="Card image cap"
+          width="300px"
+          height="300px"
+        />
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">{{profile.name}}</h5>
+          <h5 class="card-title">{{ user.name }}</h5>
           <p class="card-text">
-            {{profile.email}}
+            {{ user.email }}
           </p>
           <ul class="list-unstyled list-inline">
-            <li><strong>{{profile.commentsCount}}</strong> 已評論餐廳</li>
-            <li><strong>{{profile.favoritedRestaurantsCount}}</strong> 收藏的餐廳</li>
-            <li><strong>{{profile.followersCount}}</strong> followings (追蹤者)</li>
-            <li><strong>{{profile.followingsCount}}</strong> followers (追隨者)</li>
+            <li>
+              <strong>{{ user.commentsCount }}</strong> 已評論餐廳
+            </li>
+            <li>
+              <strong>{{ user.favoritedRestaurantsCount }}</strong> 收藏的餐廳
+            </li>
+            <li>
+              <strong>{{ user.followersCount }}</strong> followings (追蹤者)
+            </li>
+            <li>
+              <strong>{{ user.followingsCount }}</strong> followers (追隨者)
+            </li>
           </ul>
-          <p>  
-            <router-link v-if="isCurrentUser"
-            :to="{ name: 'user-edit', params: { id: profile.id }}"><button type="submit" class="btn btn-primary"
-            >edit</button></router-link>
+          <p>
+            <router-link
+              v-if="isCurrentUser"
+              :to="{ name: 'user-edit', params: { id: user.id } }"
+              ><button type="submit" class="btn btn-primary">
+                edit
+              </button></router-link
+            >
             <button
-              v-if ="isFollowed"
+              v-if="isFollowed"
               type="button"
               class="btn btn-danger btn-border mr-2"
               @click.stop.prevent="unfollow"
@@ -44,38 +57,40 @@
         </div>
       </div>
     </div>
-  </div>  
+  </div>
 </template>
 <script>
+import { emptyImageFilter } from './../utils/mixins'
 
 export default {
-  name:"UserProfileCard",
+  name: 'UserProfileCard',
+  mixins: [emptyImageFilter],
   props: {
-    profile: {
+    user: {
       style: Object,
       require: true
     },
     initialIsFollowed: {
-      style:Boolean,
+      style: Boolean,
       require: true
     },
     isCurrentUser: {
-      style:Boolean,
+      style: Boolean,
       require: true
-    },
+    }
   },
-  data () {
+  data() {
     return {
       isFollowed: this.initialIsFollowed
     }
   },
-  methods:{
-    follow () {
+  methods: {
+    follow() {
       this.isFollowed = true
     },
-    unfollow () {
+    unfollow() {
       this.isFollowed = false
-    },
+    }
   }
 }
 </script>
