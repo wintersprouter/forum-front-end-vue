0bf14b612c71fab1002d894337e3cc38cec1b7d5
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-03 22:43:47 +0800

    refactor: replace getToken in all APIs with axios interceptors

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -1,25 +1,16 @@
 import { apiHelper } from './../utils/helpers'
-// 取出 LocalStorage 裡的 token，並回傳給 getToken
-const getToken = () => localStorage.getItem('token')
 
 export default {
   getRestaurants ({ page, categoryId }) {
     // 利用物件建構子，在使用時要用 new 的方式來產生實例
     const searchParams = new URLSearchParams({ page, categoryId })
     // 透過 URLSearchParams.toString() 來產生 queryString
-    return apiHelper.get(`/restaurants?${searchParams.toString()}`, {
-      // http header 攜帶 token
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/restaurants?${searchParams.toString()}`)
   },
   getTopRestaurants () {
-    return apiHelper.get('/restaurants/top', {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get('/restaurants/top')
   },
   getDetail ({ restaurantId }) {
-    return apiHelper.get(`/restaurants/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/restaurants/${restaurantId}`)
   }
 }
