1f72fb730d18b5c3e05d3b3bef9778f93213a4c4
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-04 16:03:29 +0800

    refactor: modify RestaurantsTop ui

--- src/views/RestaurantsTop.vue
+++ src/views/RestaurantsTop.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">
-      人氣餐廳
-    </h1>
-    <RestaurantsTopCard
-      v-for="restaurant in restaurants"
-      :key="restaurant.id"
-      :initial-restaurant="restaurant"
-    />
-    <hr />
+    <Spinner v-if="isLoading" />
+    <template v-else>
+      <h1 class="mt-5">
+        人氣餐廳
+      </h1>
+      <RestaurantsTopCard
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+        :initial-restaurant="restaurant"
+      />
+      <hr />
+    </template>
   </div>
 </template>
 
@@ -17,30 +20,34 @@
 import NavTabs from './../components/NavTabs'
 import RestaurantsTopCard from './../components/RestaurantsTopCard'
 import restaurantsAPI from './../apis/restaurants'
-
-import { Toast } from'./../utils/helpers'
+import Spinner from './../components/Spinner'
+import { Toast } from './../utils/helpers'
 
 export default {
-  name:"RestaurantsTop",
+  name: 'RestaurantsTop',
   components: {
     NavTabs,
-    RestaurantsTopCard
+    RestaurantsTopCard,
+    Spinner
   },
-  data () {
+  data() {
     return {
-      restaurants:[]
+      restaurants: [],
+      isLoading: true
     }
   },
-  created () {
+  created() {
     this.fetchTopRestaurants()
   },
   methods: {
     async fetchTopRestaurants() {
       try {
-        const { data }  = await restaurantsAPI.getTopRestaurants()
+        const { data } = await restaurantsAPI.getTopRestaurants()
         const { restaurants } = data
         this.restaurants = restaurants
+        this.isLoading = false
       } catch (error) {
+        this.isLoading = false
         Toast.fire({ icon: 'error', title: '無法取得餐廳資料，請稍後再試' })
         console.log('error', error)
       }
