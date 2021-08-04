b9938c08edb71903b9823e302193f3f74a222f8e
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-04 16:06:05 +0800

    refactor: modify RestaurantsFeeds ui

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">
-      最新動態
-    </h1>
-    <hr />
-    <div class="row">
-      <div class="col-md-6">
-        <h3>最新餐廳</h3>
-        <NewestRestaurants :restaurants="restaurants" />
+    <Spinner v-if="isLoading" />
+    <template v-else>
+      <h1 class="mt-5">
+        最新動態
+      </h1>
+      <hr />
+      <div class="row">
+        <div class="col-md-6">
+          <h3>最新餐廳</h3>
+          <NewestRestaurants :restaurants="restaurants" />
+        </div>
+        <div class="col-md-6">
+          <h3>最新評論</h3>
+          <NewestComments :comments="comments" />
+        </div>
       </div>
-      <div class="col-md-6">
-        <h3>最新評論</h3>
-        <NewestComments :comments="comments" />
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -24,35 +27,42 @@ import NewestRestaurants from './../components/NewestRestaurants.vue'
 import NewestComments from './../components/NewestComments.vue'
 import feedsAPI from './../apis/feeds'
 import { Toast } from './../utils/helpers'
+import Spinner from './../components/Spinner'
 
 export default {
   components: {
     NavTabs,
     NewestRestaurants,
-    NewestComments
+    NewestComments,
+    Spinner
   },
-  data () {
+  data() {
     return {
-    restaurants: [],
-    comments: []
-  }
-},
-created () {
-  this.fetchFeeds()
-},
-methods: {
-  async fetchFeeds () {
-    try {
-      const response = await feedsAPI.getFeeds()
-      const { restaurants, comments } = response.data
-      this.restaurants = restaurants
-      this.comments = comments.filter(comment => comment.Restaurant && comment.text)
-    } catch (error) {
-      Toast.fire({
-        icon: 'error',
-        title: '無法取得餐廳資料，請稍後再試'
-      })
-      console.log('error', error)
+      restaurants: [],
+      comments: [],
+      isLoading: true
+    }
+  },
+  created() {
+    this.fetchFeeds()
+  },
+  methods: {
+    async fetchFeeds() {
+      try {
+        const response = await feedsAPI.getFeeds()
+        const { restaurants, comments } = response.data
+        this.restaurants = restaurants
+        this.comments = comments.filter(
+          comment => comment.Restaurant && comment.text
+        )
+        this.isLoading = false
+      } catch (error) {
+        this.isLoading = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+        console.log('error', error)
       }
     }
   }
