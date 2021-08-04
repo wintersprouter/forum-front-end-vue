ac70a32c2b938cf5d6fe18e0390764b9ef0ac1c9
Chia-hui <bornonefreekiss@alum.ccu.edu.tw>
2021-08-04 16:30:44 +0800

    refactor: modify AdminRestaurant ui

--- src/views/AdminRestaurant.vue
+++ src/views/AdminRestaurant.vue
@@ -1,53 +1,59 @@
 <template>
   <div class="container py-5">
-    <div class="row">
-      <div class="col-md-12">
-        <h1>{{ restaurant.name }}</h1>
-        <span class="badge badge-secondary mt-1 mb-3">
-          {{ restaurant.categoryName }}
-        </span>
-      </div>
-      <div class="col-md-4">
-        <img
-          class="img-responsive center-block"
-          :src="restaurant.image | emptyImage"
-          style="width: 250px; margin-bottom: 25px"
-        />
-        <div class="well">
-          <ul class="list-unstyled">
-            <li>
-              <strong>Opening Hour:</strong>
-              {{ restaurant.openingHours }}
-            </li>
-            <li>
-              <strong>Tel:</strong>
-              {{ restaurant.tel }}
-            </li>
-            <li>
-              <strong>Address:</strong>
-              {{ restaurant.address }}
-            </li>
-          </ul>
+    <Spinner v-if="isLoading" />
+    <template v-else>
+      <div class="row">
+        <div class="col-md-12">
+          <h1>{{ restaurant.name }}</h1>
+          <span class="badge badge-secondary mt-1 mb-3">
+            {{ restaurant.categoryName }}
+          </span>
+        </div>
+        <div class="col-md-4">
+          <img
+            class="img-responsive center-block"
+            :src="restaurant.image | emptyImage"
+            style="width: 250px; margin-bottom: 25px"
+          />
+          <div class="well">
+            <ul class="list-unstyled">
+              <li>
+                <strong>Opening Hour:</strong>
+                {{ restaurant.openingHours }}
+              </li>
+              <li>
+                <strong>Tel:</strong>
+                {{ restaurant.tel }}
+              </li>
+              <li>
+                <strong>Address:</strong>
+                {{ restaurant.address }}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <p>{{ restaurant.description }}</p>
         </div>
       </div>
-      <div class="col-md-8">
-        <p>{{ restaurant.description }}</p>
-      </div>
-    </div>
-    <hr />
-    <button type="button" class="btn btn-link" @click="$router.back()">
-      回上一頁
-    </button>
+      <hr />
+      <button type="button" class="btn btn-link" @click="$router.back()">
+        回上一頁
+      </button>
+    </template>
   </div>
 </template>
 <script>
 import { emptyImageFilter } from './../utils/mixins'
 import adminAPI from './../apis/admin'
 import { Toast } from './../utils/helpers'
+import Spinner from './../components/Spinner'
 
 export default {
   name: 'AdminRestaurant',
   mixins: [emptyImageFilter],
+  components: { Spinner },
+
   data() {
     return {
       restaurant: {
@@ -59,7 +65,8 @@ export default {
         tel: '',
         address: '',
         description: ''
-      }
+      },
+      isLoading: true
     }
   },
   mounted() {
@@ -98,7 +105,10 @@ export default {
           address,
           description
         }
+        this.isLoading = false
       } catch (error) {
+        this.isLoading = false
+
         Toast.fire({ icon: 'error', title: '無法取得餐廳資料，請稍後再試' })
       }
     }
